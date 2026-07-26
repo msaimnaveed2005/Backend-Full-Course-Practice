0798d44739542821bfce8b0efdbd6178b2804db3
Muhammad Saim Naveed <msaimnaveed5384@gmail.com>
2026-07-26 13:31:38 +0500

    chore: add nodemon dev dependency

--- Chapter_2/server.js
+++ Chapter_2/server.js
@@ -1,5 +1,7 @@
+//URL->https://localhost:8383
+//IP->10.2.0.2:8383
 const express=require('express');
 const app=express();
-const PORT=3000;
-
+const PORT=8383;
+console.log('Server is running on port '+PORT);
 app.listen(PORT,()=>console.log('Server is running on port '+PORT));
